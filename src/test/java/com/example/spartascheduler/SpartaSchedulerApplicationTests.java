7d25479f1902d9036b163f529f92388146934ffe
soo59599 <sooh59599@gmail.com>
2025-07-31 09:50:12 +0900

    Chore: 과제 초기세팅

---
+++ src/test/java/com/example/spartascheduler/SpartaSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartascheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
