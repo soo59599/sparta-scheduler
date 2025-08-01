affd3cba896544b5e9636cc33de67f796a61f398
soo59599 <sooh59599@gmail.com>
2025-08-01 11:56:47 +0900

    feat: CommentResponseDto 에서 필요없는 필드 제거

--- src/main/java/com/example/spartascheduler/dto/comment/CommentResponseDto.java
+++ src/main/java/com/example/spartascheduler/dto/comment/CommentResponseDto.java
@@ -8,14 +8,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CommentResponseDto {
 
-    private Long scheduleId;
-
     private String name;
 
     private String content;
 
     public CommentResponseDto(Comment comment) {
-        this.scheduleId = comment.getScheduleId();
         this.name = comment.getName();
         this.content = comment.getContent();
     }
